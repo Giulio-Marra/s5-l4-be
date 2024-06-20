9f23dba71793541374e43c308a8464a7ad2ed24e
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-20 14:37:03 +0200

    start project

---
+++ s5-l4-be/src/main/java/giulio_marra/s5_l4_be/S5L4BeApplication.java
@@ -0,0 +1,13 @@
+package giulio_marra.s5_l4_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5L4BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5L4BeApplication.class, args);
+	}
+
+}
---
+++ s5-l4-be/src/test/java/giulio_marra/s5_l4_be/S5L4BeApplicationTests.java
@@ -0,0 +1,13 @@
+package giulio_marra.s5_l4_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5L4BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
